e386761e6cae7f4c47a3879e36272c6999554394
Nathan Reynolds <38576090+TheTrueCoder@users.noreply.github.com>
2020-10-31 16:27:23 +1300

    Updated startvm.ps1
    Replaced quick vm setup with full config from azpowershell docs plus some modifications for naming and vm size.

--- src/startvm.ps1
+++ src/startvm.ps1
@@ -1,15 +1,41 @@
 $InstanceSize = $VMSizes[$VMNo]
 $InstanceSize
 
-New-AzVM `
-  -ResourceGroupName $ResourceGroup `
-  -Name $VMName `
-  -Location $Location `
-  -Size $InstanceSize `
-  -Image $ImageId `
-  -VirtualNetworkName "$VMName`_Vnet" `
-  -SubnetName "$VMName`_Subnet" `
-  -SecurityGroupName "$VMName`_NetworkSecurityGroup" `
-  -PublicIpAddressName "$VMName`_PublicIp" `
-  -Credential $creds `
-  -OpenPorts 3389
+# Variables for common values
+$resourceGroup = $ResourceGroup
+$location = $Location
+$vmName = $VMName
+$cred = $creds
+
+# Create a subnet configuration
+$subnetConfig = New-AzVirtualNetworkSubnetConfig -Name $brandName`_Subnet -AddressPrefix 192.168.1.0/24
+
+# Create a virtual network
+$vnet = New-AzVirtualNetwork -ResourceGroupName $resourceGroup -Location $location `
+  -Name $brandName`_Network -AddressPrefix 192.168.0.0/16 -Subnet $subnetConfig
+
+# Create a public IP address and specify a DNS name
+$pip = New-AzPublicIpAddress -ResourceGroupName $resourceGroup -Location $location `
+  -Name $vmName`_$brandName$ -DomainNameLabel $vmName`-$brandName$(Get-Random -Maximum 99) -AllocationMethod Static -IdleTimeoutInMinutes 4
+
+# Create an inbound network security group rule for port 3389
+$nsgRuleRDP = New-AzNetworkSecurityRuleConfig -Name nsgRDPRule  -Protocol Tcp `
+  -Direction Inbound -Priority 1000 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * `
+  -DestinationPortRange 3389 -Access Allow
+
+# Create a network security group
+$nsg = New-AzNetworkSecurityGroup -ResourceGroupName $resourceGroup -Location $location `
+  -Name $brandName`_NSG -SecurityRules $nsgRuleRDP
+
+# Create a virtual network card and associate with public IP address and NSG
+$nic = New-AzNetworkInterface -Name $vmName`_NIC -ResourceGroupName $resourceGroup -Location $location `
+  -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $pip.Id -NetworkSecurityGroupId $nsg.Id
+
+# Create a virtual machine configuration
+$vmConfig = New-AzVMConfig -VMName $vmName -VMSize $InstanceSize | `
+Set-AzVMOperatingSystem -Windows -ComputerName $vmName -Credential $cred | `
+Set-AzVMSourceImage -Id $ImageId | `
+Add-AzVMNetworkInterface -Id $nic.Id
+
+# Create a virtual machine
+New-AzVM -ResourceGroupName $resourceGroup -Location $location -VM $vmConfig
