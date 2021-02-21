647d357d5e0c6d7f70e1106212805c395bab95aa
Nathan Reynolds <38576090+TheTrueCoder@users.noreply.github.com>
2021-02-21 13:47:08 +1300

    Simplify templates

--- src/startTemplate.ps1
+++ src/startTemplate.ps1
@@ -1,3 +1,4 @@
 $Location = "australiaeast"
+$creds = Get-Credential -Message "Choose your login credentials for the VM"
 $ResourceGroup = New-AzResourceGroup -Name "CoolStuff" -Location $Location
-New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroup -TemplateFile .\templates\vm-template.json -TemplateParameterFile .\templates\vm.setup.json
+New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroup.ResourceGroupName -TemplateFile .\templates\vm-template.json -TemplateParameterFile .\templates\vm.setup.json -adminUsername $creds.UserName -adminPassword $creds.Password
