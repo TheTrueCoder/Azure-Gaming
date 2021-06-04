c243871b92f2143626970c81b07c70317e4e6892
TheTrueCoder <38576090+TheTrueCoder@users.noreply.github.com>
2021-06-04 14:34:49 +1200

    Update main.tf and variables.tf

---
+++ src/terraform/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.appname
+  location = "australiaeast"
+  tags = {
+    App = var.appname
+    ProjectURL = var.projecturl
+  }
+}
---
+++ src/terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "appname" {
+  type = string
+  default = "Azure-Gaming"
+}
+
+variable "projecturl" {
+  type = string
+  default = "https://github.com/TheTrueCoder/Azure-Gaming"
+}
